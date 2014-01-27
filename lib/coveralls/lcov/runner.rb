40b540ef4d18461d8bf04a106b6ec8b7059ca670
Kenji Okimoto <okimoto@clear-code.com>
2014-01-27 11:35:11 +0900

    Do not use coveralls-ruby gem
    Because it has many dependencies.

--- lib/coveralls/lcov/runner.rb
+++ lib/coveralls/lcov/runner.rb
@@ -1,8 +1,13 @@
 require "optparse"
-require "coveralls"
+require "json"
+require "net/http"
 require "coveralls/lcov/converter"
 
 module Coveralls
+  # URI: https://coveralls.io/api/v1/jobs
+  HOST = "coveralls.io"
+  PATH = "/api/v1/jobs"
+
   module Lcov
     class Runner
       def initialize(argv)
@@ -39,9 +44,32 @@ BANNER
         converter = Converter.new(tracefile)
         payload = converter.convert
         payload[:repo_token] = @repo_token if @repo_token
-        puts payload.to_json if @verbose
+        payload_json = payload.to_json
+        puts payload_json if @verbose
         unless @dry_run
-          Coveralls::API.post_json("jobs", payload)
+          post(payload_json)
+        end
+      end
+
+      def post(payload)
+        Net::HTTP.version_1_2
+
+        http = Net::HTTP.new(HOST, 443)
+        http.use_ssl = true
+        http.start do
+          request = Net::HTTP::Post.new(PATH)
+          request["content-type"] = "multipart/form-data; boundary=boundary"
+          request.body = <<BODY.gsub(/\n/, "\r\n")
+--boundary
+content-disposition: form-data; name="json_file"; filename="payload.json"
+
+#{payload}
+
+--boundary--
+BODY
+          response = http.request(request)
+          p response
+          puts response.body
         end
       end
     end
